c3b412fe77889e21fccf8baa535a51a259990791
Dongseong Hwang <dongseong.hwang@intel.com>
2016-06-02 15:25:20 +0300

    Build fix for Chromium
    Some platforms use different version of libusb. For instance, Chromium compiles
    libusb by itself. So hard coding of libuse version (i.e. <libusb-1.0/libusb.h>) causes
    compile failure.

--- include/libuvc/libuvc.h
+++ include/libuvc/libuvc.h
@@ -6,7 +6,7 @@ extern "C" {
 #endif
 
 #include <stdio.h> // FILE
-#include <libusb-1.0/libusb.h>
+#include <libusb.h>
 #include <libuvc/libuvc_config.h>
 
 /** UVC error types, based on libusb errors
