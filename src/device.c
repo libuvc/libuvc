da73c7595b904e399bf13591108b693e5e450c10
Robert Huber <lobsterman.bgsu@gmail.com>
2017-07-22 05:48:42 -0400

    Update device.c
    reduce alerts which end user perceives as more important than it actually is

--- src/device.c
+++ src/device.c
@@ -1462,8 +1462,7 @@ uvc_error_t uvc_parse_vs(
     break;
   default:
     /** @todo handle JPEG and maybe still frames or even DV... */
-    fprintf ( stderr, "unsupported descriptor subtype: %d\n",
-        descriptor_subtype );
+    //fprintf ( stderr, "unsupported descriptor subtype: %d\n",descriptor_subtype );
     break;
   }
 
