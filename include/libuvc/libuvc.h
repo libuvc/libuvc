c219e12501246aac67d662dae6fb6b712f66b0fb
erba <eric.bachard@free.fr>
2018-12-27 23:30:15 +0100

    Fixed typo

--- include/libuvc/libuvc.h
+++ include/libuvc/libuvc.h
@@ -447,7 +447,7 @@ typedef struct uvc_frame {
   /** Frame number (may skip, but is strictly monotonically increasing) */
   uint32_t sequence;
   /** Estimate of system time when the device started capturing the image */
-  uvc_timeval_t timeval capture_time;
+  uvc_timeval_t capture_time;
   /** Handle on the device that produced the image.
    * @warning You must not call any uvc_* functions during a callback. */
   uvc_device_handle_t *source;
