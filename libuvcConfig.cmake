e3431c0499ee86e20e7ab138732711df7e4eae0d
Dmitriy Lekomtsev <lekomtsev@gmail.com>
2019-07-15 16:00:39 +0300

    First working CMake config after modernize

---
+++ libuvcConfig.cmake
@@ -0,0 +1,15 @@
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
+
+unset(extraArgs)
+if(${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY)
+  list(APPEND extraArgs QUIET)
+endif()
+if(${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED)
+  list(APPEND extraArgs REQUIRED)
+endif()
+
+find_package(JpegPkg ${extraArgs})
+find_package(LibUSB ${extraArgs})
+include("${CMAKE_CURRENT_LIST_DIR}/libuvcTargets.cmake")
+
+set(libuvc_FOUND TRUE)
