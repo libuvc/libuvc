610b66c98a7dc10b1201a343ae29f73e3a168af9
Dmitriy Lekomtsev <lekomtsev@gmail.com>
2019-07-16 14:13:30 +0300

    Append comments and more robust OpenCV searching

--- libuvcConfig.cmake
+++ libuvcConfig.cmake
@@ -1,5 +1,7 @@
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 
+# If find_package called with REQUIRED or QUIET we need to
+# forward down it
 unset(extraArgs)
 if(${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY)
   list(APPEND extraArgs QUIET)
