972fae0db7fbf7296a4e29054802dc6d41851e4a
Dmitry Mikushin <dmitry@kernelgen.org>
2020-03-10 02:35:13 +0100

    Adding uvc_mjpeg2gray (#137)
    * Adding uvc_mjpeg2gray, refactoring frame-mjpeg.c to share common code between uvc_mjpeg2rgb and uvc_mjpeg2gray

--- include/libuvc/libuvc.h
+++ include/libuvc/libuvc.h
@@ -791,6 +791,7 @@ uvc_error_t uvc_yuyv2uv(uvc_frame_t *in, uvc_frame_t *out);
 
 #ifdef LIBUVC_HAS_JPEG
 uvc_error_t uvc_mjpeg2rgb(uvc_frame_t *in, uvc_frame_t *out);
+uvc_error_t uvc_mjpeg2gray(uvc_frame_t *in, uvc_frame_t *out);
 #endif
 
 #ifdef __cplusplus
