1545b2bf040592a13bbb1029c974f8f9fa79c9ba
Zig Globulin <zig@zigsystem.com>
2020-09-25 01:46:46 +0200

    add support for mjpeg frames in uvc_any2rgb

--- src/frame.c
+++ src/frame.c
@@ -440,6 +440,10 @@ uvc_error_t uvc_uyvy2bgr(uvc_frame_t *in, uvc_frame_t *out) {
  */
 uvc_error_t uvc_any2rgb(uvc_frame_t *in, uvc_frame_t *out) {
   switch (in->frame_format) {
+#ifdef LIBUVC_HAS_JPEG
+    case UVC_FRAME_FORMAT_MJPEG:
+      return uvc_mjpeg2rgb(in, out);
+#endif
     case UVC_FRAME_FORMAT_YUYV:
       return uvc_yuyv2rgb(in, out);
     case UVC_FRAME_FORMAT_UYVY:
