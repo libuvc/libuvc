ff0ac0a5430ae10e9f86878f0102fd2434a188cb
Bradley Benninger <bbenninger@aeryon.com>
2021-05-04 13:34:53 -0400

    uvc_stream_start: first call to libusb_submit_transfer can fail
    was not entering the 'Clean up transfer buffers when an error occurs'
    part when an error occurs with the first transfer

--- src/stream.c
+++ src/stream.c
@@ -1236,8 +1236,9 @@ uvc_error_t uvc_stream_start(
     }
   }
 
-  if ( ret != UVC_SUCCESS && transfer_id > 0 ) {
-    for ( ; transfer_id < LIBUVC_NUM_TRANSFER_BUFS; transfer_id++) {
+  if ( ret != UVC_SUCCESS && transfer_id >= 0 ) {
+    for ( ; transfer_id < LIBUVC_NUM_TRANSFER_BUFS;
+        transfer_id++) {
       free ( strmh->transfers[transfer_id]->buffer );
       libusb_free_transfer ( strmh->transfers[transfer_id]);
       strmh->transfers[transfer_id] = 0;
